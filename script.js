a0ee447dd555bc6ce2c32d4f442f4b38b64515a9
piapandora <165914977+piapandora@users.noreply.github.com>
2024-04-15 02:29:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,20 +17,17 @@ menu.addEventListener("mouseenter", function() {
     menu.style.opacity = 1;
     colors.style.opacity = 1;
     document.body.style.transition = 'background 1s ease';
-    // document.body.style.background = '#222222'
 })
 
 colors.addEventListener("mouseenter", function() {
     menu.style.opacity = 1;
     colors.style.opacity = 1;
     document.body.style.transition = 'background 1s ease';
-    // document.body.style.background = '#222222'
 })
 
 btnH.addEventListener('click', () => {  
     menu.style.opacity = 0
     colors.style.opacity = 0
-    // document.body.style.background = '#222222'
 })
 
 btnF.addEventListener('click', () => {
