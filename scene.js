479cbcad15a7f427f3f3cf5f2ced5ab99d2d7f52
piapandora <165914977+piapandora@users.noreply.github.com>
2024-05-05 02:53:58 +0300

    Add files via upload

---
+++ scene.js
@@ -0,0 +1,39 @@
+let currentScene = 0;
+const totalScenes = document.getElementById("scenes").children.length;
+refreshFooter();
+
+function nextScene() {
+  if (currentScene < (totalScenes - 1)){
+  document.getElementById(`scene${currentScene}`).style.display = "none";
+  currentScene = currentScene + 1;
+  document.getElementById(`scene${currentScene}`).style.display = "block";
+  refreshFooter();
+  }
+}
+
+function prevScene() {
+  if (currentScene > 0){
+  document.getElementById(`scene${currentScene}`).style.display = "none";
+  currentScene = currentScene - 1;
+  document.getElementById(`scene${currentScene}`).style.display = "block";
+  refreshFooter();
+  }
+}
+
+function refreshFooter(){
+  document.getElementById('message').textContent = sceneMessages[currentScene];
+  
+  if (currentScene == 0){
+    document.getElementById("prev-btn").classList.add('footer-btn-inactive');
+  }
+  else {
+    document.getElementById("prev-btn").classList.remove('footer-btn-inactive');
+  }
+  
+  if (currentScene == (totalScenes - 1)){
+    document.getElementById("next-btn").classList.add('footer-btn-inactive');
+  }
+  else {
+    document.getElementById("next-btn").classList.remove('footer-btn-inactive');
+  }
+}  
