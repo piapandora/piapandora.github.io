7e305ac5be3600ad3c8bf506174942c4de905661
piapandora <165914977+piapandora@users.noreply.github.com>
2026-04-30 00:36:49 +0300

    Update code.js

--- box/code.js
+++ box/code.js
@@ -11,30 +11,24 @@ async function handle() {
         timer = setTimeout(() => { count = 0; timerStart = false; }, 2666);
     }
     if (++count < reqs) return;
-
     clearTimeout(timer);
     count = 0;
     timerStart = false;
     if (isBusy) return;
     isBusy = true;
-
     try {
         const status = document.getElementById('code');
         const getRes = await fetch(`${WORKER_URL}?key=pandoraToggle`, { headers: { 'X-Vault-Key': pass } });
         let currentState = getRes.ok ? (await getRes.text()).trim() : "0";
-        
         const newState = currentState === "1" ? "0" : "1";
-
         const postRes = await fetch(`${WORKER_URL}?key=pandoraToggle`, { 
             method: 'POST', 
             headers: { 'X-Vault-Key': pass }, 
             body: newState 
         });
-
         if (postRes.ok) {
             !!+newState ? console.log("✔") : console.log("✖");
         }
-
     } catch (e) {
         console.error("SYNC ERROR");
     } finally {
