1262d5242476257c2718861716125f14f1703865
piapandora <165914977+piapandora@users.noreply.github.com>
2026-09-25 11:49:26 +0300

    Add files via upload

---
+++ deck/deck.js
@@ -0,0 +1,79 @@
+images = [
+  // 2s
+  "https://assets.piapandora.com/deck/2-of-clubs.png",
+  "https://assets.piapandora.com/deck/2-of-diamonds.png",
+  "https://assets.piapandora.com/deck/2-of-hearts.png",
+  "https://assets.piapandora.com/deck/2-of-spades.png",
+
+  // 3s
+  "https://assets.piapandora.com/deck/3-of-clubs.png",
+  "https://assets.piapandora.com/deck/3-of-diamonds.png",
+  "https://assets.piapandora.com/deck/3-of-hearts.png",
+  "https://assets.piapandora.com/deck/3-of-spades.png",
+
+  // 4s
+  "https://assets.piapandora.com/deck/4-of-clubs.png",
+  "https://assets.piapandora.com/deck/4-of-diamonds.png",
+  "https://assets.piapandora.com/deck/4-of-hearts.png",
+  "https://assets.piapandora.com/deck/4-of-spades.png",
+
+  // 5s
+  "https://assets.piapandora.com/deck/5-of-clubs.png",
+  "https://assets.piapandora.com/deck/5-of-diamonds.png",
+  "https://assets.piapandora.com/deck/5-of-hearts.png",
+  "https://assets.piapandora.com/deck/5-of-spades.png",
+
+  // 6s
+  "https://assets.piapandora.com/deck/6-of-clubs.png",
+  "https://assets.piapandora.com/deck/6-of-diamonds.png",
+  "https://assets.piapandora.com/deck/6-of-hearts.png",
+  "https://assets.piapandora.com/deck/6-of-spades.png",
+
+  // 7s
+  "https://assets.piapandora.com/deck/7-of-clubs.png",
+  "https://assets.piapandora.com/deck/7-of-diamonds.png",
+  "https://assets.piapandora.com/deck/7-of-hearts.png",
+  "https://assets.piapandora.com/deck/7-of-spades.png",
+
+  // 8s
+  "https://assets.piapandora.com/deck/8-of-clubs.png",
+  "https://assets.piapandora.com/deck/8-of-diamonds.png",
+  "https://assets.piapandora.com/deck/8-of-hearts.png",
+  "https://assets.piapandora.com/deck/8-of-spades.png",
+
+  // 9s
+  "https://assets.piapandora.com/deck/9-of-clubs.png",
+  "https://assets.piapandora.com/deck/9-of-diamonds.png",
+  "https://assets.piapandora.com/deck/9-of-hearts.png",
+  "https://assets.piapandora.com/deck/9-of-spades.png",
+
+  // 10s
+  "https://assets.piapandora.com/deck/10-of-clubs.png",
+  "https://assets.piapandora.com/deck/10-of-diamonds.png",
+  "https://assets.piapandora.com/deck/10-of-hearts.png",
+  "https://assets.piapandora.com/deck/10-of-spades.png",
+
+  // Jacks
+  "https://assets.piapandora.com/deck/jack-of-clubs.png",
+  "https://assets.piapandora.com/deck/jack-of-diamonds.png",
+  "https://assets.piapandora.com/deck/jack-of-hearts.png",
+  "https://assets.piapandora.com/deck/jack-of-spades.png",
+
+  // Queens
+  "https://assets.piapandora.com/deck/queen-of-clubs.png",
+  "https://assets.piapandora.com/deck/queen-of-diamonds.png",
+  "https://assets.piapandora.com/deck/queen-of-hearts.png",
+  "https://assets.piapandora.com/deck/queen-of-spades.png",
+
+  // Kings
+  "https://assets.piapandora.com/deck/king-of-clubs.png",
+  "https://assets.piapandora.com/deck/king-of-diamonds.png",
+  "https://assets.piapandora.com/deck/king-of-hearts.png",
+  "https://assets.piapandora.com/deck/king-of-spades.png",
+
+  // Aces
+  "https://assets.piapandora.com/deck/ace-of-clubs.png",
+  "https://assets.piapandora.com/deck/ace-of-diamonds.png",
+  "https://assets.piapandora.com/deck/ace-of-hearts.png",
+  "https://assets.piapandora.com/deck/ace-of-spades.png"
+];
